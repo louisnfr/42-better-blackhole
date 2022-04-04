0bdbddf1bc18617707a5d31613c02e96d9b567fe
Louis Raffin <louis.raffin@gmail.com>
2022-04-04 23:30:51 +0200

    Working on buttons in popup

--- popup.js
+++ popup.js
@@ -2,31 +2,59 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	// EXTENSION
 
-	document.querySelector("#enable").addEventListener("click", function () {
-		document.getElementById("enable").hidden = true;
-		document.getElementById("disable").hidden = false;
+	document.querySelector("#enable_ext").addEventListener("click", function () {
+		toggleEnabled(true, settingsSavedReloadMessage);
 	});
 
-	document.querySelector("#disable").addEventListener("click", function () {
-		document.getElementById("enable").hidden = false;
-		document.getElementById("disable").hidden = true;
+	document.querySelector("#disable_ext").addEventListener("click", function () {
+		toggleEnabled(false, settingsSavedReloadMessage);
 	});
 
 	// EMOTES
 
-	document.querySelector("#enable_emotes").addEventListener("click", function () {
-		document.getElementById("enable_emotes").hidden = true;
-		document.getElementById("disable_emotes").hidden = false;
-	});
+	// document.querySelector("#enable_emotes").addEventListener("click", function () {
+	// 	document.getElementById("enable_emotes").hidden = true;
+	// 	document.getElementById("disable_emotes").hidden = false;
+	// });
 
-	document.querySelector("#disable_emotes").addEventListener("click", function () {
-		document.getElementById("enable_emotes").hidden = false;
-		document.getElementById("disable_emotes").hidden = true;
-	});
+	// document.querySelector("#disable_emotes").addEventListener("click", function () {
+	// 	document.getElementById("enable_emotes").hidden = false;
+	// 	document.getElementById("disable_emotes").hidden = true;
+	// });
 
 	// ABOUT
 
 	document.querySelector("#about").addEventListener("click", function () {
 		window.open("https://github.com/louisnfr/42-better-blackhole");
 	});
+
+	function toggleEnabled(enabled, callback) {
+		chrome.storage.sync.set(
+		{
+			enabled: enabled
+		},
+		function () {
+			toggleEnabledUI(enabled);
+			if (callback) callback(enabled);
+		}
+		);
+	}
+
+	function toggleEnabledUI(enabled) {
+		document.querySelector("#enable_ext").classList.toggle("hide", enabled);
+		document.querySelector("#disable_ext").classList.toggle("hide", !enabled);
+	}
+
+	function settingsSavedReloadMessage(enabled) {
+		setStatusMessage(
+		  `${enabled ? "Enabled" : "Disabled"}. Reload page to see changes`
+		);
+	  }
+
+	  function setStatusMessage(str) {
+		const status_element = document.querySelector("#status");
+		status_element.classList.toggle("hide", false);
+		status_element.innerText = str;
+	  }
+
 });
