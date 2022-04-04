37ba8f212c28495ae7d795261aada73e4faadf1a
Louis Raffin <louis.raffin@gmail.com>
2022-04-04 23:00:40 +0200

    Fix popup toggle buttons

--- popup.js
+++ popup.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-	document.querySelector("#about").addEventListener("click", function () {
-		window.open("https://github.com/louisnfr/42-better-blackhole");
-	});
+	// EXTENSION
 
 	document.querySelector("#enable").addEventListener("click", function () {
-		toggleEnabled(true, settingsSavedReloadMessage);
+		document.getElementById("enable").hidden = true;
+		document.getElementById("disable").hidden = false;
 	});
 
 	document.querySelector("#disable").addEventListener("click", function () {
-		toggle();
+		document.getElementById("enable").hidden = false;
+		document.getElementById("disable").hidden = true;
 	});
 
-	function toggle()
-	{
-		var t = document.getElementById("disable");
-		t.hidden;
-	}
+	// EMOTES
+
+	document.querySelector("#enable_emotes").addEventListener("click", function () {
+		document.getElementById("enable_emotes").hidden = true;
+		document.getElementById("disable_emotes").hidden = false;
+	});
+
+	document.querySelector("#disable_emotes").addEventListener("click", function () {
+		document.getElementById("enable_emotes").hidden = false;
+		document.getElementById("disable_emotes").hidden = true;
+	});
+
+	// ABOUT
+
+	document.querySelector("#about").addEventListener("click", function () {
+		window.open("https://github.com/louisnfr/42-better-blackhole");
+	});
 });
